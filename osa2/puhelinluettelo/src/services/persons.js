d6f5e60282c6db61350c2c89f897ad07bab4e919
Markus Röman <markus.roman@hotmail.fi>
2019-12-26 08:02:04 +0200

    uibuild

--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -1,20 +1,22 @@
-import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+import axios from "axios";
+const baseUrl = "/persons";
 
 const getAll = () => {
-  return axios.get(baseUrl).then(response => response.data)
-}
+  return axios.get(baseUrl).then(response => response.data);
+};
 
 const create = newObject => {
-  return axios.post(baseUrl, newObject).then(response => response.data)
-}
+  return axios.post(baseUrl, newObject).then(response => response.data);
+};
 
 const update = (id, newObject) => {
-  return axios.put(`${baseUrl}/${id}`, newObject).then(response => response.data)
-}
+  return axios
+    .put(`${baseUrl}/${id}`, newObject)
+    .then(response => response.data);
+};
 
-const remove = (id) => {
-    return axios.delete(`${baseUrl}/${id}`).then(response => response.data)
-}
+const remove = id => {
+  return axios.delete(`${baseUrl}/${id}`).then(response => response.data);
+};
 
-export default { getAll, create, update, remove }
+export default { getAll, create, update, remove };
